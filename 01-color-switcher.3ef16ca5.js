1208d7598a335a2c32a5e5e8648703e4d1a36724
hodakivskaK <96114298+hodakivskaK@users.noreply.github.com>
2023-03-20 15:27:43 +0000

    Deploying to gh-pages from @ hodakivskaK/goit-js-hw-09@18bf9e4f1e132124837c7834ead703bf5df198e0 🚀

---
+++ 01-color-switcher.3ef16ca5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(function(){r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.3ef16ca5.js.map
